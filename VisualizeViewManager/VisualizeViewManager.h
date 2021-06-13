df89f4a97a1c6f678b1fa3eb47f3330cbf9ca892
shota_xx <58303571+shotaxx00@users.noreply.github.com>
2021-06-14 05:02:56 +0900

    [Add] Add Xcode Project

---
+++ VisualizeViewManager/VisualizeViewManager.h
@@ -0,0 +1,18 @@
+//
+//  VisualizeViewManager.h
+//  VisualizeViewManager
+//
+//  Created by 長野祥太 on 2021/06/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisualizeViewManager.
+FOUNDATION_EXPORT double VisualizeViewManagerVersionNumber;
+
+//! Project version string for VisualizeViewManager.
+FOUNDATION_EXPORT const unsigned char VisualizeViewManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisualizeViewManager/PublicHeader.h>
+
+
